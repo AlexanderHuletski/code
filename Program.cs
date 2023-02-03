9b3a283b359d708773684ceecd6ee4c5ac80aaed
Huletski Alexander <alexander.huletski@gmail.com>
2023-02-03 14:41:57 +0300

    was added Parse method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿var word = "Hello,world!";
 
 Console.WriteLine(word);
+
+
+int numberOne = int.Parse(Console.ReadLine());
+var numberTwo = 8;
+var result = numberOne + numberTwo;
+Console.WriteLine(result);
